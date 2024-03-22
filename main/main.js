d45b827157f85f7cbca87305482b7ac316f0a309
spnazar <123718085+spnazar@users.noreply.github.com>
2024-03-22 14:35:37 +0500

    Add files via upload

---
+++ main/main.js
@@ -0,0 +1,73 @@
+// первое задание 
+let firstName = "nazar";
+let lastName = "spirin";
+let fullName = firstName + lastName;
+console.log(fullName);
+
+// второе задание
+let a = 5;
+let b = 7;
+let c = 3;
+
+// математические операций
+let sum = a + b;
+let d = a - b;
+let p = a * b;
+let q = a / b;
+let r = a % b;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+//третье задание
+let isTrue = true;
+let isFalse = false;
+
+console.log(isTrue && isFalse);
+console.log(isTrue || isFalse);
+console.log(!isTrue);
+
+//четвертое задание
+let counter = 10;
+
+counter++;
+console.log("инкремент:", counter);
+
+counter--;
+console.log("декремент:", counter);
+
+//пятое задание
+let greeting = "Привет";
+let name = 'nazar';
+let message = greeting + name;
+
+console.log(message);
+
+//шестое задание
+let prefix = "Привет, ";
+let middle = "как дела?";
+let suffix = "Хорошо!";
+
+let offer = prefix + middle + suffix;
+
+console.log(offer);
+
+//седьмое задание
+let num1 = 2;
+let num2 = 34;
+let result = "Результат: " + (num1 + num2);
+
+console.log(result);
+
+//восьмое задание
+let line = "текстовая строка";
+
+
+let upCase = line.toUpperCase();
+console.log("верхний регистр:", upCase);
+
+let lowerCase = line.toLowerCase();
+console.log("нижний регистр:", lowerCase);
